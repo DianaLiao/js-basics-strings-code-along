c6a424024dc2e18cbfdb3cc1879d5a3f97d02b7c
Diana Liao <dijali@gmail.com>
2021-04-05 21:31:14 -0400

    commit tree a58fa8718e7e3f2b5c531fed86fd80151e719d83

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code in this file!
+
+const currentUser = 'Sqkrcat'
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+console.log( "We" + ' ' + `can` + " " + 'concat' + `enate` + " as many strings " + 'as our heart ' + `desires.`)
+
+const myString = 'template literal';
+
+const myNumber = 10;
+
+const myBoolean = false;
+
+console.log(`Saying that interpolation with ${myString}s is better than concatenation ${90 + myNumber}% of the time is simply ${myBoolean}. But it is pretty cool!`)
+
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+
